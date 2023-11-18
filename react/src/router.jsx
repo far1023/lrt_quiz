2336e16188d52f35006776236670a7b8da5b205c
far1023 <fuadagilr@gmail.com>
2023-11-19 00:26:13 +0700

    add route to dashboard and quizes

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,9 +1,31 @@
-import { createBrowserRouter } from "react-router-dom";
-import AuthLayout from "./views/AuthLayout";
+import { Navigate, createBrowserRouter } from "react-router-dom";
+
+import AuthLayout from "./views/layouts/AuthLayout";
+import DLayout from "./views/layouts/DLayout";
+import Dashboard from "./views/Dashboard";
 import LoginForm from "./components/LoginForm";
+import Quiz from "./views/Quiz";
 import SignupForm from "./components/SignupForm";
 
 const router = createBrowserRouter([
+  {
+    path: "/home",
+    element: <DLayout />,
+    children: [
+      {
+        path: "",
+        element: <Navigate to="dashboard" />,
+      },
+      {
+        path: "dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "quizes",
+        element: <Quiz />,
+      },
+    ],
+  },
   {
     path: "/",
     element: <AuthLayout />,
