c759c8316cf75d973a2e92e3ac9d527250088e98
far1023 <fuadagilr@gmail.com>
2023-11-18 22:37:12 +0700

    login signup layout

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,10 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "./App";
+import AuthLayout from "./views/AuthLayout";
+import LoginForm from "./components/LoginForm";
+import SignupForm from "./components/SignupForm";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "/login",
+        element: <LoginForm />,
+      },
+      {
+        path: "/signup",
+        element: <SignupForm />,
+      },
+    ],
   },
 ]);
 
